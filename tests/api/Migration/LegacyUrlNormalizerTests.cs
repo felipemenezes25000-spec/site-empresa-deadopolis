14ebbe2ae06527a13439e53b98cc69b9f124d912
felipemenezes25000-spec <felipemenezes25000@gmail.com>
2026-08-26 12:50:02 -0300

    test(migration): cover noisy home query canonicalization

--- tests/api/Migration/LegacyUrlNormalizerTests.cs
+++ tests/api/Migration/LegacyUrlNormalizerTests.cs
@@ -6,6 +6,8 @@ public sealed class LegacyUrlNormalizerTests
 {
     [Theory]
     [InlineData("http://www.deodapolis.ms.gov.br/index.php", "/index.php")]
+    [InlineData("https://www.deodapolis.ms.gov.br/index.php?foo=bar&patt=noise", "/index.php")]
+    [InlineData("https://deodapolis.ms.gov.br/?-project-veresen-feat=raya&patt=vena-cava", "/")]
     [InlineData("https://deodapolis.ms.gov.br//e-sic/diario.php?tipo=1&utm_source=x", "/e-sic/diario.php?tipo=1")]
     [InlineData("/sec.php?tipo=10", "/sec.php?tipo=10")]
     public void NormalizePreservesMeaningfulLegacyPath(string input, string expected)
